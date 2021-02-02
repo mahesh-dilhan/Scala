b8d5e6065bdea1e93f305e39ecb537edb308740d
mahesh <mail.mahesh.lk@gmail.com>
2021-02-02 13:05:57 +0800

    Scala Basic

---
+++ ScalaBasics/src/main/scala/ScalaArray.scala
@@ -0,0 +1,10 @@
+object ScalaArray {
+
+  def main(args: Array[String]): Unit = {
+      var namesOflang  = Array[String]("Go", "Scala","Java");
+
+    for (e<-namesOflang) {
+      println(e);
+    }
+  }
+}
